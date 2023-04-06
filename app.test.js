1a5151b33c426e269bf659c8c64406511f8ac208
Alyssa Denary <alyssa.denary@gmail.com>
2023-04-05 20:29:16 -0400

    add test for patch

--- app.test.js
+++ app.test.js
@@ -29,8 +29,8 @@ describe("POST /items", function () {
       price: 50
     }
     const resp = await request(app)
-    .post("/items")
-    .send(product2);
+      .post("/items")
+      .send(product2);
     expect(resp.body).toEqual({added: product2});
     console.log("items:", items);
     // TODO: potential test to add - but getting error of items not
@@ -44,12 +44,42 @@ describe("POST /items", function () {
 
 describe("GET /items/:name", function () {
   test("valid", async function () {
-    const resp = await request(app).get("/items/product1");
+    const resp = await request(app).get(`/items/${PRODUCT1.name}`);
     expect(resp.body).toEqual(PRODUCT1);
   });
 
   test("invalid", async function () {
     const resp = await request(app).get("/items/product9");
+    expect(resp.status).toEqual(404);
+    expect(resp.body).toEqual({
+      "error": {
+        "message": "Not Found",
+        "status": 404
+      }
+    });
+  });
+});
+
+describe("PATCH /items/:name", function () {
+  test("valid change all", async function () {
+    const patchData = {name:"Troll", price: 1.00};
+    const resp = await request(app)
+      .patch(`/items/${PRODUCT1.name}`)
+      .send(patchData);
+    expect(resp.body).toEqual({updated: patchData});
+  });
+
+  test("valid change one", async function () {
+    const resp = await request(app)
+      .patch(`/items/${PRODUCT1.name}`)
+      .send({price: 1});
+    expect(resp.body).toEqual({updated: {name: PRODUCT1.name, price: 1}});
+  });
+
+  test("invalid doesn't exist", async function () {
+    const resp = await request(app)
+      .patch("/items/product9")
+      .send({price: 1});
     expect(resp.status).toEqual(404);
     expect(resp.body).toEqual({
       "error": {
